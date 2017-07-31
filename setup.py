09b7770ad468c98d1b1233848db2f475c70d5e5c
hadrienj <hadrienjean@gmail.com>
2017-07-31 14:36:49 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='EEG',
+      version='0.1',
+      description='EEG analysis utils',
+      url='https://github.com/hadrienj/EEG',
+      author='hadrienj',
+      author_email='hadrienjean@gmail.com',
+      license='MIT',
+      packages=['EEG'],
+      zip_safe=False)
